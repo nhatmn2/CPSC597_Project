5849238bea337db395d32b31e7e6d10e4bf8d8be
nhatmn2 <nhatmn2@csu.fullerton.edu>
2024-03-26 22:13:53 -0700

    Commit the change of authentication

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,25 +1,34 @@
-import Head from 'next/head';
-import styles from '@/styles/Home.module.css';
+import Head from 'next/head'
+import styles from '@/styles/Home.module.css'
 import Dashboard from "@/pages/dashboard";
-import Header from '@/pages/components/Header';
-import SideMenu from '@/pages/components/SideMenu';
-export default function Home() {
-  return (
-    <>
-    <Head>
-      <title>phishing email dashboard</title>
-      <meta name="description" content="phishing email dashboard" />
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
-      
-    <main className={styles.main}>
-      <Dashboard/>
-      <Header/>
-      <SideMenu/>
+import Header from "@/components/Header";
+import SideMenu from "@/components/SideMenu";
+import Login from "@/components/Login";
+import {useSession} from "next-auth/react";
 
-    </main>
-    </>
-    
-  );
+export default function Home() {
+    const { data: session } = useSession();
+
+    return (
+        <>
+            <Head>
+                <title>Data Dashboard</title>
+                <meta name="description" content="Data Dashboard" />
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <main className={styles.main}>
+                <Header />
+                {
+                    session && (
+                    <>
+                        <SideMenu />
+                        <Dashboard />
+                    </>
+                    )
+                }
+                <Login />
+            </main>
+        </>
+    )
 }
