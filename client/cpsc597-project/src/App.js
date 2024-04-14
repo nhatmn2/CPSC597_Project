a621505d4fb2d1bf8f8662f998ecd6df42d7f683
nhatmn2 <nhatmn2@csu.fullerton.edu>
2024-04-14 12:05:24 -0700

    updating the routing for each of the pages

--- client/cpsc597-project/src/App.js
+++ client/cpsc597-project/src/App.js
@@ -1,7 +1,16 @@
-
-import Topbar from "./scenes/global/Topbar";
 import { ColorModeContext, useMode } from "./theme";
 import { CssBaseline, ThemeProvider } from "@mui/material";
+import { Routes, Route } from "react-router-dom";
+import Topbar from "./scenes/global/Topbar"; /*this is for the top horizontal nav bar*/
+import Sidebar from "./scenes/global/Sidebar"; /*this is for the left side vertical menu*/
+import Dashboard from "./scenes/dashboard";
+import Team from "./scenes/team";
+import FAQ from "./scenes/faq";
+import Dataset from "./scenes/dataset";
+import ContentCheck from "./scenes/contentcheck";
+import DomainCheck from "./scenes/domaincheck";
+import Contact from "./scenes/contact";
+import Calendar from "./scenes/calendar";
 
 function App() {
   const [theme, colorMode] = useMode();
@@ -12,6 +21,17 @@ function App() {
       <div className = 'app'>
         <main className='content'>
           <Topbar/>
+          {/* Routing all the pages */}
+          <Routes>
+            <Route path="/" element={<Dashboard/>} />
+            <Route path="/team" element={<Team />} />
+            <Route path="/contact" element={<Contact/>} />
+            <Route path="/dataset" element={<Dataset/>} />
+            <Route path="/contentcheck" element={<ContentCheck/>} />
+            <Route path="/domaincheck" element={<DomainCheck/>} />
+            <Route path="/faq" element={<FAQ/>} />
+            <Route path="/calendar" element={<Calendar/>}/>
+          </Routes>
         </main>
       </div>
     </ThemeProvider>
--- client/cpsc597-project/src/scenes/global/Topbar.jsx
+++ client/cpsc597-project/src/scenes/global/Topbar.jsx
@@ -16,16 +16,16 @@ const Topbar = () => {
     const colorMode = useContext(ColorModeContext);
 
     return (
-        <Box display="flex" justifyContent="space-between" p={2}>
+        <Box display="flex" justifyContent="space-between" alignItems="center" p={2}>
             {/* SEARCH BAR */}
-            <Box display="flex" backgroundColor={colors.primary[400]} borderRadius="3px">
-                <InputBase sx={{ ml: 2, flex: 1 }} placeholder="Search" />
-                <IconButton type="button" sx={{ p: 1 }}>
-                    <SearchIcon/>
+            <Box display="flex" alignItems="center" backgroundColor={colors.primary[400]} borderRadius="3px">
+                <InputBase sx={{ ml: 1, flex: 1 }} placeholder="Search" />
+                <IconButton type="button" sx={{ p: '10px' }}>
+                    <SearchIcon />
                 </IconButton>
             </Box>
             {/* ICONS TOP RIGHT */}
-            <Box display="flex">
+            <Box display="flex" gap={1}>
                 <IconButton onClick={colorMode.toggleColorMode}>
                     {theme.palette.mode === "dark" ? (<DarkModeOutlinedIcon />) : (<LightModeOutlinedIcon />)}
                 </IconButton>
