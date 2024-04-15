d566ce88ab5aba48d018f9622ac329673fa35a1c
nhatmn2 <nhatmn2@csu.fullerton.edu>
2024-04-14 19:05:26 -0700

    adding the sidebar

--- client/cpsc597-project/src/App.js
+++ client/cpsc597-project/src/App.js
@@ -1,36 +1,39 @@
-import { ColorModeContext, useMode } from "./theme";
+import { useState } from "react";
+import { Routes } from "react-router-dom";
+import Topbar from "./scenes/global/Topbar";
+import CustomSidebar from "./scenes/global/Sidebar";
+//import Dashboard from "./scenes/dashboard";
 import { CssBaseline, ThemeProvider } from "@mui/material";
-import { Routes, Route } from "react-router-dom";
-import Topbar from "./scenes/global/Topbar"; /*this is for the top horizontal nav bar*/
-import Sidebar from "./scenes/global/Sidebar"; /*this is for the left side vertical menu*/
-import Dashboard from "./scenes/dashboard";
-import Team from "./scenes/team";
-import FAQ from "./scenes/faq";
-import Dataset from "./scenes/dataset";
-import ContentCheck from "./scenes/contentcheck";
-import DomainCheck from "./scenes/domaincheck";
-import Contact from "./scenes/contact";
-import Calendar from "./scenes/calendar";
+import { ColorModeContext, useMode } from "./theme";
+//import Team from "./scenes/team";
+//import FAQ from "./scenes/faq";
+//import Dataset from "./scenes/dataset";
+//import ContentCheck from "./scenes/contentcheck";
+//import DomainCheck from "./scenes/domaincheck";
+//import Contacts from "./scenes/contacts";
+//import Calendar from "./scenes/calendar";
 
 function App() {
   const [theme, colorMode] = useMode();
+  const [isSidebar, setIsSidebar] = useState(true);
   
   return (<ColorModeContext.Provider value={colorMode}>
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <div className = 'app'>
+        <CustomSidebar isSidebar={isSidebar}/>
         <main className='content'>
-          <Topbar/>
+          <Topbar setIsSidebar={setIsSidebar}/>
           {/* Routing all the pages */}
           <Routes>
-            <Route path="/" element={<Dashboard/>} />
-            <Route path="/team" element={<Team />} />
-            <Route path="/contact" element={<Contact/>} />
+            {/*<Route path="/" element={<Dashboard/>} />*/}
+            {/*<Route path="/contact" element={<Contacts/>} />*/}
+            {/*<Route path="/team" element={<Team />} />
             <Route path="/dataset" element={<Dataset/>} />
             <Route path="/contentcheck" element={<ContentCheck/>} />
             <Route path="/domaincheck" element={<DomainCheck/>} />
             <Route path="/faq" element={<FAQ/>} />
-            <Route path="/calendar" element={<Calendar/>}/>
+            <Route path="/calendar" element={<Calendar/>}/>*/}
           </Routes>
         </main>
       </div>
@@ -41,3 +44,4 @@ function App() {
 }
 
 export default App;
+
---
+++ client/cpsc597-project/src/scenes/dashboard/index.js
@@ -0,0 +1,10 @@
+import { Box, Button, IconButton, Typography, useTheme } from "@mui/material";
+import { tokens } from "../../theme";
+import { mockTransactions } from "../../data/mockData";
+import DownloadOutlinedIcon from "@mui/icons-material/DownloadOutlined";
+const Dashboard = () => {
+    const theme = useTheme();
+    const colors = tokens(theme.palette.mode);
+}
+
+export default Dashboard
